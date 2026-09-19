a802ec0b9ebc35dbedb18fac35e0ed116e6957db
nickoff <nick.olehnovich@gmail.com>
2026-09-19 08:47:31 +0300

    feat: implement countVowels

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -299,8 +299,18 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const initialValue = 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'y'];
+  return str
+    .toLowerCase()
+    .split('')
+    .reduce((count, currentValue) => {
+      if (vowels.includes(currentValue)) {
+        return count + 1;
+      }
+      return count;
+    }, initialValue);
 }
 
 /**
